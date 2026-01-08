a56055c1d30a0b0c55458c816ed61fa1750f02cf
warrenryo <warrenryo@gmail.com>
2026-01-09 00:27:41 +0800

    modified database.php 2

--- config/database.php
+++ config/database.php
@@ -56,9 +56,9 @@ return [
             'prefix_indexes' => true,
             // 'strict' => true,
             // 'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
+            'options' => extension_loaded('pdo_mysql') ? [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            ] : [],
         ],
 
         'mariadb' => [
