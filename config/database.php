0ef3aea458f1c7ef935ccb37060572a23f94bd0c
warrenryo <warrenryo@gmail.com>
2026-01-10 11:57:47 +0800

    moved mysql attr on .env

--- config/database.php
+++ config/database.php
@@ -45,21 +45,23 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST'),
-            'port' => env('DB_PORT', 4000),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', 3306),
             'database' => env('DB_DATABASE'),
             'username' => env('DB_USERNAME'),
             'password' => env('DB_PASSWORD'),
-            'charset' => env('utf8mb4'),
-            'collation' => env('utf8mb4_unicode_ci'),
+            'unix_socket' => env('DB_SOCKET'),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
-            // 'strict' => true,
-            // 'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? [
-                PDO::MYSQL_ATTR_SSL_CA => '/etc/ssl/certs/ca-certificates.crt',
-            ] : [],
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
         ],
+
 
         // 'mysql' => [
         //     'driver' => 'mysql',
